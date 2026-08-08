99dca6d4611a36a7d518d3cf2eb05b4f6e806a4c
amieljohn0714 <bernalesamieljohn@gmail.com>
2026-08-08 10:46:19 +0800

    Fix QR ID assignment in onScanSuccess function

--- app.js
+++ app.js
@@ -1,136 +1,69 @@
 const API_URL =
 "https://script.google.com/macros/s/AKfycbyunRcLrGqxqZVmsj6BMiMsS4ga1FnO951o7bHS9_OdpySAGqJiX164DCRL1avxLmIK4A/exec";
 
-
 let scanner;
 let busy = false;
 
-
 function showMessage(html){
-
     document.getElementById("result").innerHTML = html;
-
 }
-
-
 async function onScanSuccess(decodedText){
-
     if(busy){
         return;
     }
-
     busy = true;
-
-
     try {
-
         const response = await fetch(API_URL, {
-
             method: "POST",
-
             body: JSON.stringify({
-
-                qrID: decodedText
-
+             qrID: decodedText
             })
-
         });
-
-
         const result = await response.json();
 
-
-
         if(result.success){
-
             showMessage(`
-
             <div class="success">
-
             ✅ Attendance Recorded
-
             </div>
-
             <br>
-
             ${result.message}
-
             `);
-
         }
-
         else{
-
             showMessage(`
-
             <div class="error">
-
             ❌ ${result.message}
-
             </div>
-
             `);
-
         }
-
-
     }
-
     catch(error){
-
         showMessage(`
-
         <div class="error">
-
         ❌ Connection Error
-
         </div>
-
         <br>
-
         ${error.message}
-
         `);
-
     }
-
 
     setTimeout(()=>{
-
         busy=false;
-
         showMessage("Ready to Scan...");
-
-
     },2000);
-
-
 }
 
-
-
 async function startScanner(){
-
     try{
-
-
         scanner = new Html5Qrcode("reader");
-
-
         await scanner.start(
-
             {
-
                 facingMode:"environment"
-
             },
-
             {
-
                 fps:10,
-
               qrbox: function(viewfinderWidth, viewfinderHeight){
-
     let minEdge =
         Math.min(
             viewfinderWidth,
@@ -143,44 +76,23 @@ async function startScanner(){
     };
 
 },
-
             onScanSuccess
-
         );
 
-
         showMessage("Ready to Scan...");
-
-
     }
-
-
     catch(error){
-
         showMessage(`
-
         <div class="error">
-
         Camera Error
-
         </div>
-
         <br>
-
         ${error}
-
         `);
-
         console.error(error);
-
     }
-
 }
 
-
-
 window.onload=function(){
-
     startScanner();
-
 };
